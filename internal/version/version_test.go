d06836ca7c806e3e2668574bbfbaa546c01fed6d
Tom Proctor <tomhjp@users.noreply.github.com>
2021-02-04 15:41:07 +0000

    Fix test

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -9,13 +9,14 @@ import (
 func TestGetVersion(t *testing.T) {
 	BuildDate = "Now"
 	BuildVersion = "version"
+	GoVersion = "go version x.y.z"
 
 	v, err := GetVersion()
 
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
 	}
-	expected := fmt.Sprintf(`{"version":"version","buildDate":"Now","minDriverVersion":"%s"}`, minDriverVersion)
+	expected := fmt.Sprintf(`{"version":"version","buildDate":"Now","goVersion":"go version x.y.z","minDriverVersion":"%s"}`, minDriverVersion)
 	if !strings.EqualFold(v, expected) {
 		t.Fatalf("string doesn't match, expected %s, got %s", expected, v)
 	}
