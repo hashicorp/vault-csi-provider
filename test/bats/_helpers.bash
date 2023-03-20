d2268b363e9ec9139b2115f03681b7413aa92a49
Tom Proctor <tomhjp@users.noreply.github.com>
2023-03-20 16:30:07 +0000

    Hopefully address flaky acceptance test due to slow image pull

--- test/bats/_helpers.bash
+++ test/bats/_helpers.bash
@@ -14,13 +14,16 @@ wait_for_success() {
 }
 
 setup_postgres() {
-    # Setup postgres
+    # Setup postgres, pulling the image first to help avoid CI timeouts.
+    POSTGRES_IMAGE="$(awk '/image:/{print $NF}' $CONFIGS/postgres.yaml)"
+    docker pull "${POSTGRES_IMAGE}"
+    kind load docker-image "${POSTGRES_IMAGE}"
     POSTGRES_PASSWORD=$(openssl rand -base64 30)
     kubectl --namespace=test create secret generic postgres-root \
         --from-literal=POSTGRES_USER="root" \
         --from-literal=POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
     kubectl --namespace=test apply -f $CONFIGS/postgres.yaml
-    kubectl wait --namespace=test --for=condition=Ready --timeout=5m pod -l app=postgres
+    kubectl wait --namespace=test --for=condition=Ready --timeout=10m pod -l app=postgres
 
     # Configure vault to manage postgres
     kubectl --namespace=csi exec vault-0 -- vault secrets enable database
